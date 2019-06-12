eb4fb349ebd15669883eadebad4c4b8dacb7f62e
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-06-12 13:24:25 +0200

    Updates

---
+++ precache-manifest.726e82f460683d5a6336a46913e79a02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a442112902ae9b6903a0",
+    "url": "/minesweeper/static/css/main.5d3f74ec.chunk.css"
+  },
+  {
+    "revision": "a442112902ae9b6903a0",
+    "url": "/minesweeper/static/js/main.18043b2b.chunk.js"
+  },
+  {
+    "revision": "0f86e2ffe97a36d3984a",
+    "url": "/minesweeper/static/js/runtime~main.dd26f679.js"
+  },
+  {
+    "revision": "aca48ef3b0cd23f472f5",
+    "url": "/minesweeper/static/js/2.16dbea45.chunk.js"
+  },
+  {
+    "revision": "68538c60bfa0f5e9a00b2076606b57cd",
+    "url": "/minesweeper/index.html"
+  }
+];
